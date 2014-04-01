6f2a2c9d52f04ba6154c468915d0c70020740b63
Chavjoh <j.chavaillaz@gmail.com>
2014-04-01 15:16:38 +0200

    Add start template and composer.

---
+++ app/Form.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Form to generate dictionary
+ *
+ * @package DictionaryGeneratorPHP
+ * @author Chavaillaz Johan
+ * @since 1.0.0
+ * @license Apache 2.0 License
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+
+	<title>PHP Benchmark</title>
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<!-- jQuery -->
+	<script type="text/javascript" src="package/jquery/jquery-2.1.0.min.js"></script>
+
+	<!-- Bootstrap -->
+	<link href="package/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+
+	<!-- Main design -->
+	<link href="style/design.css" rel="stylesheet" />
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<h3 class="text-muted">DictionaryGeneratorPHP</h3>
+		</div>
+
+		<div class="jumbotron">
+			<h1>Dictionary Generator</h1>
+
+			<p class="lead">
+				...
+			</p>
+		</div>
+
+		<form action="" method="post">
+			<div class="row marketing">
+				<div class="col-lg-6">
+					<fieldset>
+						<legend>Basic alphabet</legend>
+						<label><input type="checkbox" name="alphabet" value="letters" checked="checked" />Letters</label> <br />
+						<label><input type="checkbox" name="alphabet" value="numbers" checked="checked" />Numbers</label> <br />
+						<label><input type="checkbox" name="alphabet" value="special" />Special characters</label>
+						<!-- - _ , . : ; + " * # % & / ( ) = ? ` ' ^ ! $ [ ] { } < > @ -->
+					</fieldset>
+
+					<fieldset>
+						<legend>Size</legend>
+						<label>Minimum size <input type="text" name="minimum" /> <br /></label>
+						<label>Maximum size <input type="text" name="maximum" /></label>
+					</fieldset>
+				</div>
+
+				<div class="col-lg-6">
+					<fieldset>
+						<legend>Results</legend>
+						<label><input type="radio" name="result" value="show" checked="checked" />Show results in page</label> <br />
+						<label><input type="radio" name="result" value="download" />Download results</label> <br />
+					</fieldset>
+					<fieldset>
+						<legend>Launch</legend>
+						Start generator. It could take awhile. <br />
+						<input type="submit" name="submit" value="Generate dictionary" />
+					</fieldset>
+				</div>
+			</div>
+		</form>
+		<div id="loading">
+			<img src="image/loading.gif" alt="Loading" />
+		</div>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Dictionary generator execution
+ *
+ * @package DictionaryGeneratorPHP
+ * @author Chavaillaz Johan
+ * @since 1.0.0
+ * @license Apache 2.0 License
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// It could take a lot of time
+set_time_limit(0);
+
+// Hard mode here :D
+error_reporting(E_ALL);
+
+// Define directories
+define("ROOT", dirname(__FILE__).'/');
+define("PATH_APP", ROOT.'app/');
+
+// Load packages
+include_once './package/autoload.php';
+
+// Include classes
+require_once(PATH_APP.'DictionaryGenerator.php');
+
+if (isset($_POST['submit']))
+{
+    // Get settings
+
+    // Launch generator
+    $dictionary = new DictionaryGenerator(/* Settings */);
+    $dictionary->execute();
+}
+else
+{
+    // Show generator form
+    require_once(PATH_APP.'Form.php');
+}
